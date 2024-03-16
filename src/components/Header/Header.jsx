1edc8242dcf0ff509187276e0e6b70881f0035c6
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-16 20:58:50 +0600

    route setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li> <NavLink to='/'>Home</NavLink> </li>
+      <li> <NavLink to='/donation'>Donation</NavLink> </li>
+      <li> <NavLink to='/statistics'>Statistics</NavLink> </li> 
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Root from "./components/Root/Root.jsx";
+import Error from "./components/Error/Error.jsx";
+import Donation from "./components/Donation/Donation.jsx";
+import Statistics from "./components/Statistics/Statistics.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/donation",
+        element: <Donation></Donation>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
